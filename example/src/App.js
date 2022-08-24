930c1a82bd403197966b600732cfeeee943fb0e0
VengalreddyAkhila <akhila.it1801@gmail.com>
2022-08-24 17:58:12 +0530

    [added the class components seperately]

--- example/src/App.js
+++ example/src/App.js
@@ -1,31 +1,44 @@
-
+import React, { Component } from 'react';
 import classes from './App.module.css';
+import ProductData from './Utills/ProductData';
 
-import ProductDetails from './ProductDetails';
-import ProductPreview from './ProductPreview';
+import ProductDetails from './ProductDetails/ProductDetails';
+import ProductPreview from './ProductPreview/ProductPreview';
+import Topbar from './Topbar/Topbar';
 
-function App() {
+class App extends Component {
+  state = {
+    productData:ProductData,
+    currentPreviewImage:'https://imgur.com/xSIK4M8.png',
+    showHeartBeatSection:true,
+  }
+onColorOptionClick = (pos) => {
+  const updatedPreviewImage = this.state.productData.colorOptions[pos].imageUrl
+  console.log(updatedPreviewImage)
+  this.setState({currentPreviewImage:updatedPreviewImage})
+}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <nav className = {classes.Topbar}>
-       <img src="https://i.dlpng.com/static/png/197684_preview.png" alt = "" />
-     </nav>
-      </header>
-      <div className={classes.MainConatiner}>
-        <div className={classes.ProductPreview}>
-        <ProductPreview/>
+  render() {
+    return (
+      <div className="App">
+        <Topbar />
+
+        <div className={classes.MainConatiner}>
+          <div className={classes.ProductPreview}>
+            <ProductPreview currentPreviewImage={this.state.currentPreviewImage}
+            showHeartBeatSection={this.state.showHeartBeatSection}/>
           </div>
-        
-        <div className={classes.ProductData}>
-          <ProductDetails/>
-        
+
+          <div className={classes.ProductData}>
+            <ProductDetails  data={this.state.productData} onColorOptionClick=
+            {this.onColorOptionClick}/>
+
+          </div>
         </div>
       </div>
-      </div>
-    
-  );
+
+    );
+  }
 }
 
 export default App;
---
+++ example/src/ProductDetails/ProductDetails.js
@@ -0,0 +1,50 @@
+import React  from "react";
+
+import classes from './ProductDetails.module.css'
+//import ProductData  from "./ProductData";
+
+ const ProductDetails = (props) => {
+   console.log(props.data)
+   const colorOptions = props.data.colorOptions.map((item,pos) => {
+      const classArr = [classes.ProductImage]
+      if(pos === 0){
+         classArr.push(classes.SelectProductImage);
+      }
+      return(
+         <img key={pos} className={classes.ProductImage} src={item.imageUrl} 
+         alt={item.styleName} onClick={() => props.onColorOptionClick(pos)}></img>
+      )
+   })
+
+   const featureList = props.data.featureList.map((item,pos) => {
+      const classArr = [classes.FeatureItem];
+      if(pos === 0){
+         classArr.push(classes.SelectedFeaturedItem)
+      }
+      return(
+         <button key={pos} className={classArr.join(' ')}>{item}</button>
+      )
+   })
+
+    return(
+        <div className={classes.ProductData}>
+        <h1 className={classes.ProductTitle}>{props.data.title}</h1>
+        <p className={classes.ProductDescription}>{props.data.description}</p>
+        <h3 className={classes.SectionHeading}>Select Color</h3>
+        <div>
+         {colorOptions}
+         </div>
+      
+           <h3 className={classes.SectionHeading}>Features</h3>
+           <div >
+          {featureList}
+           </div>
+           <button className={classes.PrimaryButton}>Buy Now</button>
+      
+        </div>
+
+    )
+
+ }
+
+ export default ProductDetails;
